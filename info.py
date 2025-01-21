51f06573d2b6c861fb4646e0bff0aa0fbd6bce1a
Sahil96386 <122726309+Sahil96386@users.noreply.github.com>
2025-01-21 22:16:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,7 +88,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Ind_supportgp')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Indmoviegp')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Ind_supportgp')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Ind_supportgp') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Sahilrjs')
